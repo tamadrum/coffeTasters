4b0c079039157c41026310c818b071d22aa17951
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-02-22 06:45:13 -0300

    ios

---
+++ CoffeeTastersIOS/CoffeeTastersIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeTastersIOS
+//
+//  Created by Ettore Luglio on 2/22/17.
+//  Copyright © 2017 Luglio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
