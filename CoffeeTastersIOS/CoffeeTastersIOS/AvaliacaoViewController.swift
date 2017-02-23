66138c088a5596e13566bfa33a8a733eb8e7e17c
RodrigoLuglio <rodrigo.luglio@icloud.com>
2017-02-23 15:36:53 -0300

    Classes criadas - Sem cocoa

---
+++ CoffeeTastersIOS/CoffeeTastersIOS/AvaliacaoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AvaliacaoViewController.swift
+//  CoffeeTastersIOS
+//
+//  Created by Rodrigo Luglio on 23/02/17.
+//  Copyright © 2017 Luglio. All rights reserved.
+//
+
+import Foundation
+
+class AvaliacaoViewController:ViewController{
+    
+}
---
+++ CoffeeTastersIOS/CoffeeTastersIOS/ConfiguracoesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConfiguracoesViewController.swift
+//  CoffeeTastersIOS
+//
+//  Created by Rodrigo Luglio on 23/02/17.
+//  Copyright © 2017 Luglio. All rights reserved.
+//
+
+import Foundation
+
+class ConfiguracoesViewController:ViewController{
+    
+}
