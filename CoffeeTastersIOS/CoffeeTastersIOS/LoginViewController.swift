c4141e00f42f048a16ce01b4ce25d8d5689e9d22
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-02-23 16:55:57 -0300

    ios facebook com .framework

--- CoffeeTastersIOS/CoffeeTastersIOS/LoginViewController.swift
+++ CoffeeTastersIOS/CoffeeTastersIOS/LoginViewController.swift
@@ -6,8 +6,24 @@
 //  Copyright © 2017 Luglio. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+import FBSDKLoginKit
 
 class LoginViewController:ViewController{
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var senhaTextField: UITextField!
+    
+    @IBOutlet var esqueciMinhaSenha: UIButton!
+    @IBOutlet var registrar: UIButton!
+    @IBOutlet var entrar: UIButton!
+    
+    override func viewDidLoad() {
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email"]
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+    }
     
 }
