b7542a895e33019670613e905c6de79c2820652b
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-03-02 19:24:40 -0300

    FlavorViewController como UITableViewController e a criacao de mais uma celula customizada

---
+++ CoffeeTastersIOS/CoffeeTastersIOS/FlavorCustomViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorCustomViewCell.swift
+//  CoffeeTastersIOS
+//
+//  Created by Ettore Luglio on 3/2/17.
+//  Copyright © 2017 Luglio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FlavorCustomViewCell: UITableViewCell {
+    
+    @IBOutlet weak var flavor: UILabel!
+    @IBOutlet weak var valor: UISlider!
+    
+}
