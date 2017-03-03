ad8eab0b001455465a522691bdaf74d4fd7dd0f8
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-03-03 14:39:19 -0300

    arrumando o core data

--- CoffeeTastersIOS/CoffeeTastersIOS/AvaliacoesDao.swift
+++ CoffeeTastersIOS/CoffeeTastersIOS/AvaliacoesDao.swift
@@ -93,4 +93,52 @@ class AvaliacoesDao {
         
         return avaliacoes
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    
+//    var people: [NSManagedObject] = []
+//    // como usar : person.value(forKeyPath: "name") as? String
+//    
+//    func load () {
+//        let managedContext = appDelegate.persistentContainer.viewContext
+//        
+//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+//        
+//        do {
+//            people = try managedContext.fetch(fetchRequest)
+//        } catch let error as NSError {
+//            print("Could not fetch. \(error), \(error.userInfo)")
+//        }
+//    }
+//    
+//    func save(name: String) {
+//        
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+//            return
+//        }
+//        
+//        let managedContext = appDelegate.persistentContainer.viewContext
+//        
+//        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+//        
+//        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+//        
+//        person.setValue(name, forKeyPath: "name")
+//        
+//        do {
+//            try managedContext.save()
+//            people.append(person)
+//        } catch let error as NSError {
+//            print("Could not save. \(error), \(error.userInfo)")
+//        }
+//    }
+    
 }
