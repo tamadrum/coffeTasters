a0eaff617bfa6a771a4a409101302c8bce236c41
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-03-08 19:59:03 -0300

    Tela de cadastro

--- CoffeeTastersIOS/CoffeeTastersIOS/CadastroViewController.swift
+++ CoffeeTastersIOS/CoffeeTastersIOS/CadastroViewController.swift
@@ -7,7 +7,59 @@
 //
 
 import UIKit
+import Foundation
 
 class CadastroViewController:UIViewController{
+ 
+    @IBOutlet weak var nomeTextField: UITextField?
+    @IBOutlet weak var emailTextField: UITextField?
+    @IBOutlet weak var senhaTextField: UITextField?
+    @IBOutlet weak var repeteSenhaTextField: UITextField?
     
+    override func viewDidLoad() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func btnSalvar(_ sender: Any) {
+        if ( senhaTextField?.text == "" || repeteSenhaTextField?.text == "" || nomeTextField?.text == "" || emailTextField?.text == "") {
+            let alertController = UIAlertController(title: "Erro!", message: "Nunhum dos campos podem ser vazios...", preferredStyle: .alert)
+            
+            let saveAction = UIAlertAction(title: "OK", style: .cancel, handler: {
+                alert -> Void in
+                
+            })
+            
+            alertController.addAction(saveAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            if  senhaTextField?.text != repeteSenhaTextField?.text {
+                let alertController = UIAlertController(title: "Erro!", message: "A senha e a repeticão devem ser iguais...", preferredStyle: .alert)
+            
+                let saveAction = UIAlertAction(title: "OK", style: .cancel, handler: {
+                alert -> Void in
+                
+                })
+
+                alertController.addAction(saveAction)
+            
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                let usuario = Usuario()
+            
+                usuario.nome = (nomeTextField?.text)!
+                usuario.email = (emailTextField?.text)!
+                usuario.senha = (senhaTextField?.text)!
+            
+                usuario.save()
+            
+                self.performSegue(withIdentifier: "telaPerfilPosRegistro", sender: self)
+            }
+        }
+    }
 }
