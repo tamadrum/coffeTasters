12b379adb9bd1f555f9ff398f12586a3001c812a
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-05-28 21:08:44 -0300

    Acordeon nas avaliacoes do coffeetasters

--- CoffeeTasters/CoffeeTasters/custom/AvaliacaoCustomCell.swift
+++ CoffeeTasters/CoffeeTasters/custom/AvaliacaoCustomCell.swift
@@ -16,22 +16,22 @@ class AvaliacaoCustomCell: UITableViewCell {
     @IBOutlet weak var safra: UILabel!
     @IBOutlet weak var comentario: UITextView!
     @IBOutlet weak var rating: RatingView!
-    @IBOutlet weak var flavor: WheelFlavor!
     @IBOutlet weak var nomeCafe: UILabel!
     
+    var flavor: WheelFlavor!
     var nav: UINavigationController?
     
     @IBAction func share(){
         let usuario = Usuario()
         usuario.load()
         
-        let imageToShare = UIImage.init(view: flavor)
+        //let imageToShare = UIImage.init(view: flavor)
         
         let textoParaPublicar = "\(usuario.nome) avaliou um novo café: \(nomeCafe.text!) \n Avaliação: \(rating.rating)/4.0 \n"
         
         let vc = SLComposeViewController(forServiceType:SLServiceTypeFacebook)
         vc?.setInitialText("Aonde isso vai?")
-        vc?.add(imageToShare)
+        //vc?.add(imageToShare)
         vc?.add(URL(string: "http://www.coffeetasters.com.br/")!)
         vc?.setInitialText(textoParaPublicar)
         
---
+++ CoffeeTasters/CoffeeTasters/custom/AvaliacaoDetailsCell.swift
@@ -0,0 +1,16 @@
+//
+//  AvaliacaoDetailsCell.swift
+//  CoffeeTasters
+//
+//  Created by Ettore Luglio on 28/05/17.
+//  Copyright © 2017 Tamadrum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AvaliacaoDetailsCell: UITableViewCell {
+    
+    @IBOutlet weak var flavor: WheelFlavor!
+    
+}
